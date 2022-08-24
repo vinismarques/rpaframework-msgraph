bcaada8129bbc8976d14e4bf2f4a12fd04a365e0
Blake Waud <blake@robocorp.com>
2022-08-24 15:58:53 -0400

    Replace `microsoftgraph-client` with `O365` (#4)

--- src/RPA/MSGraph.py
+++ src/RPA/MSGraph.py
@@ -1,24 +1,42 @@
 from enum import Enum
 import logging
-import secrets
 from typing import Optional, Union
-from microsoftgraph.client import Client, Users
+from O365 import Account, MSGraphProtocol, FileSystemTokenBackend, directory
+from O365.utils import Token, BaseTokenBackend
+from O365.utils.utils import (
+    ME_RESOURCE,
+    USERS_RESOURCE,
+    GROUPS_RESOURCE,
+    SITES_RESOURCE,
+)
 from robot.api.deco import keyword
+from pathlib import Path
+
 
 DEFAULT_REDIRECT_URI = "https://login.microsoftonline.com/common/oauth2/nativeclient"
+DEFAULT_TOKEN_PATH = Path("/temp")
+
+# Define scopes
+DEFAULT_PROTOCOL = MSGraphProtocol()
+BASIC_SCOPE = DEFAULT_PROTOCOL.get_scopes_for("basic")
 
 
 class PermissionBundle(Enum):
-    BASIC = ["offline_access", "user.read"]
+    BASIC = BASIC_SCOPE
 
 
 class MSGraphAuthenticationError(Exception):
     "Error when authentication fails."
 
 
+class RobocorpVaultTokenBackend(BaseTokenBackend):
+    "A simple Token backend that saves to Robocorp vault"
+    pass
+
+
 class MSGraph:
     """
-    The *MSGraph* library wraps the microsoftgraph package, giving robots
+    The *MSGraph* library wraps the `O365 package`_, giving robots
     the ability to access the Microsoft Graph API programmatically.
 
     Oauth Configuration
@@ -40,8 +58,9 @@ class MSGraph:
 
     .. TODO: Determine bundles of permissions needed for each keyword in the library.
 
+    .. _O365 package: https://pypi.org/project/O365
     .. _Oauth Graph Example Bot: https://robocorp.com/portal/
-    .. _register an app: https://portal.azure.com/#view/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/~/RegisteredApps
+    .. _register an app: https://portal.azure.com/#blade/Microsoft_AAD_RegisteredApps/ApplicationsListBlade
     .. _Microsoft Graph permissions reference: https://docs.microsoft.com/en-us/graph/permissions-reference
 
 
@@ -54,9 +73,12 @@ class MSGraph:
         self,
         client_id: Optional[str] = None,
         client_secret: Optional[str] = None,
-        user_token: Optional[str] = None,
+        token: Optional[Token] = None,
         refresh_token: Optional[str] = None,
         redirect_uri: Optional[str] = None,
+        vault_backend: bool = False,
+        vault_secret: Optional[str] = None,
+        file_backend_path: Optional[Path] = DEFAULT_TOKEN_PATH,
     ) -> None:
         """When importing the library to Robot Framework, you can set the
         ``client_id`` and ``client_secret``.
@@ -66,45 +88,50 @@ class MSGraph:
 
         """
         self.logger = logging.getLogger(__name__)
+        # TODO: Implement a `TokenBackend` that uses Robocorp vault,
+        #       if implemented, returned refresh tokens are unnecessary.
+        if not vault_backend:
+            self.token_backend = FileSystemTokenBackend(
+                file_backend_path, "auth_token.txt"
+            )
+        elif vault_backend and not vault_secret:
+            raise ValueError(
+                "Argument vault_secret cannot be blank is vault_backend set to True."
+            )
+        else:
+            raise NotImplementedError(
+                "Robocorp vault token backend not yet implemented."
+            )
         if client_id and client_secret:
-            self.client = Client(client_id, client_secret)
+            self.configure_msgraph_client(
+                client_id, client_secret, refresh_token, redirect_uri
+            )
         else:
             self.client = None
-        if redirect_uri:
-            self.redirect_uri = redirect_uri
-        else:
-            self.redirect_uri = DEFAULT_REDIRECT_URI
-        self.user_token: Optional[dict] = user_token
-        self.random_state = None
+            self.redirect_uri = redirect_uri or DEFAULT_REDIRECT_URI
 
     def _require_client(self):
         if self.client is None:
             raise MSGraphAuthenticationError("The MSGraph client is not configured.")
 
-    def _require_user_token(self):
-        if self.user_token is None:
+    def _require_authentication(self):
+        self._require_client()
+        if not self.client.is_authenticated:
             raise MSGraphAuthenticationError(
-                "There is no user access token to authenticate the request, "
-                "refresh the token with a refresh token to continue."
+                "The MS Graph client is not authenticated."
             )
 
-    @keyword
-    def generate_oauth_authorize_url(
-        self,
-        client_id: Optional[str] = None,
-        client_secret: Optional[str] = None,
-        redirect_uri: str = DEFAULT_REDIRECT_URI,
-        scope: PermissionBundle = PermissionBundle.BASIC,
-    ) -> str:
-        """Generates an authorization URL which must be opened
-        by the user to complete the OAuth flow.
+    def _get_refresh_token(self):
+        """Returns the refresh token using the backend if that backend
+        is not the Vault.
         """
-        self.random_state = secrets.token_urlsafe()
-        if not self.client:
-            self.configure_msgraph_client(client_id, client_secret)
-        return self.client.authorization_url(
-            redirect_uri, scope.value, self.random_state
-        )
+        try:
+            if isinstance(self.token_backend, FileSystemTokenBackend):
+                return self.token_backend.load_token().get("refresh_token")
+            else:
+                return None
+        except AttributeError:
+            return None
 
     @keyword
     def configure_msgraph_client(
@@ -114,87 +141,103 @@ class MSGraph:
         refresh_token: Optional[str] = None,
         redirect_uri: str = DEFAULT_REDIRECT_URI,
     ) -> Union[str, None]:
-        """Configures the MS Graph client when authorization has
-        already been completed previously. If a refresh token is
+        """Configures the MS Graph client. If a refresh token is
         known, it can be provided to obtain a current user token
         to authenticate with. A new refresh token is returned
         if one is provided.
         """
-        self.client = Client(client_id, client_secret)
+        credentials = (client_id, client_secret)
+        self.client = Account(credentials, token_backend=self.token_backend)
         self.redirect_uri = redirect_uri
         if refresh_token:
             return self.refresh_oauth_token(refresh_token)
 
     @keyword
-    def authorize_and_get_token(self, authorization_code: str) -> str:
+    def generate_oauth_authorization_url(
+        self,
+        client_id: Optional[str] = None,
+        client_secret: Optional[str] = None,
+        redirect_uri: str = DEFAULT_REDIRECT_URI,
+        scope: PermissionBundle = PermissionBundle.BASIC,
+    ) -> str:
+        """Generates an authorization URL which must be opened
+        by the user to complete the OAuth flow.
+        """
+        if not self.client:
+            self.configure_msgraph_client(
+                client_id, client_secret, redirect_uri=redirect_uri
+            )
+        return self.client.connection.get_authorization_url(
+            scope.value,
+            redirect_uri,
+        )[0]
+
+    @keyword
+    def authorize_and_get_token(self, authorization_url: str) -> str:
         # pylint: disable=anomalous-backslash-in-string
-        """Exchanges the OAuth authorization code obtained from
+        """Exchanges the OAuth authorization URL obtained from
         \`Generate OAuth authorize url\` for an access token. This
         library maintains the user access token for current requests
         and returns the refresh token to be stored in a secure location
         (e.g., the Robocorp Control Room Vault).
         """  # noqa: W605
         self._require_client()
-        # TODO: Need to help robot developer handle receiving authorization
-        #       code redirect URL and parse it into the authorization_code
-        #       and check the ``random_state``. When the user completes the
-        #       web flow, I am not sure where the authorization redirect
-        #       query is currently sent when using the default redirec uri
-        #       for native clients.
-        if not self.random_state:
-            raise ValueError(
-                "You must first generate an authorize URL and obtain an "
-                "authorization code via user interaction."
+        if self.client.connection.request_token(
+            authorization_url, redirect_uri=self.redirect_uri
+        ):
+            self.logger.info("Authentication successful.")
+            return self._get_refresh_token()
+        else:
+            raise MSGraphAuthenticationError(
+                f"Authentication not successful using '{authorization_url}' as auth URL."
             )
-        self.user_token = self.client.exchange_code(
-            self.redirect_uri, authorization_code
-        )
-        self.set_access_token(self.user_token.data)
-        return self.user_token.data["refresh_token"]
 
     @keyword
-    def refresh_oauth_token(self, refresh_token: str) -> str:
+    def refresh_oauth_token(self, refresh_token: Optional[str] = None) -> str:
         """Refreshes the user token using the provided ``refresh_token``.
         The user token is retained in the library and a new
-        refresh token is returned.
+        refresh token is returned. If no token is provided, this keyword
+        assumes the Robocorp Vault is being used as a backend and attempts
+        to refresh it based on that backend.
         """
         self._require_client()
-        self.user_token = self.client.refresh_token(self.redirect_uri, refresh_token)
-        self.set_access_token(self.user_token.data)
-        return self.user_token.data["refresh_token"]
-
-    @keyword
-    def set_access_token(self, access_token: dict) -> None:
-        """This keyword should not normally need to be called as the
-        token is set by other keywords when a user token response is
-        received, but can be used to set the access token directly.
-        """
-        self._require_client()
-        self.client.set_token(access_token)
-
-    @keyword
-    def get_me(self, properties: Union[str, list[str]] = None) -> dict:
-        """Returns the MS Graph object representing the currently logged
-        in user. You can supply a list of additional properties to
-        return as a comma-separated list or a list object.
-
-        To understand the list of available properties, refer to the
-        `Get User MS Graph API documentation`_.
-
-        .. _Get User MS Graph API documentation: https://docs.microsoft.com/en-us/graph/api/user-get?view=graph-rest-1.0&tabs=http
-
-        :param properties: A string with properties separated
-                           by commas or a list object with properties.
-
-        """
-        self._require_client()
-        self._require_user_token()
-        if properties:
-            try:
-                parsed_properties = properties.split(",")
-            except AttributeError:
-                parsed_properties = properties
-            parsed_properties = [p.strip() for p in parsed_properties]
-            return self.client.users.get_me(f"$select={','.join(parsed_properties)}")
+        if refresh_token:
+            self.token_backend.token = Token(refresh_token=refresh_token)
+            self.token_backend.save_token()
+        if self.client.connection.refresh_token():
+            self.logger.info("Token successfully refreshed.")
+            return self._get_refresh_token()
         else:
-            return self.client.users.get_me()
+            raise MSGraphAuthenticationError("Access token could not be refreshed.")
+
+    @keyword
+    def get_me(self) -> directory.User:
+        """Returns the MS Graph object representing the currently logged
+        in user. A User object is returned. Properties of the user can
+        be accessed like so:
+
+        .. code-block: robotframework
+
+            *** Tasks ***
+            Get the me object
+                ${me}=    Get me
+                ${full_name}=    Set variable    ${me.full_name}
+        """
+        self._require_authentication()
+        return self.client.get_current_user()
+
+    @keyword
+    def search_for_users(
+        self, search_string: str, resource: str = USERS_RESOURCE
+    ) -> list[directory.User]:
+        # pylint: disable=anomalous-backslash-in-string
+        """Returns a list of user objects from the Active Directory
+        based on the provided search string.
+
+        User objects have additional properties that can be accessed
+        with dot-notation, see \`Get me\` for additional details.
+        """  # noqa: W605
+        self._require_authentication()
+        directory = self.client.directory(resource)
+        query = directory.new_query().search(search_string)
+        return directory.get_users(query=query)
--- tests/python/test_msgraph.py
+++ tests/python/test_msgraph.py
@@ -1,25 +1,29 @@
+from json.encoder import JSONEncoder
+import time
 from typing import Union
-from urllib.parse import urlencode
-from mock import MagicMock
+from urllib.parse import parse_qs, urlencode, urlparse
+from mock import MagicMock, ANY
 import pytest
 from pytest_mock import MockerFixture
 from RPA.MSGraph import MSGraph, DEFAULT_REDIRECT_URI, PermissionBundle
 from pathlib import Path
+import re
 
 RESOURCE_DIR = Path(__file__).parent / "resources"
+TEMP_DIR = Path(__file__).parent / "temp"
 CONFIG_FILE = RESOURCE_DIR / "msgraph"
 
 DEFAULT_STATE = "123"
 MOCK_CLIENT_ID = "my-client-id"
 MOCK_CLIENT_SECRET = "my-client-secret"
-MOCK_AUTH_CODE = "my-user-auth-code"
+MOCK_AUTH_CODE = "https://localhost/myapp/?code=my-mock-auth-code-123&state={}&session_state=mock-session-state#"
 MOCK_ACCESS_TOKEN = "microsoft-access-token-{:0>2}"
 MOCK_REFRESH_TOKEN = "microsoft-refresh-token-{:0>2}"
 
 
 @pytest.fixture
 def library() -> MSGraph:
-    return MSGraph()
+    return MSGraph(file_backend_path=TEMP_DIR)
 
 
 @pytest.fixture
@@ -30,87 +34,123 @@ def configured_lib(library: MSGraph) -> MSGraph:
 
 @pytest.fixture
 def init_auth(library: MSGraph, mocker: MockerFixture) -> str:
-    config = {"token_urlsafe.return_value": DEFAULT_STATE}
-    mocker.patch("RPA.MSGraph.secrets", **config)
-    return library.generate_oauth_authorize_url(MOCK_CLIENT_ID, MOCK_CLIENT_SECRET)
+    return library.generate_oauth_authorization_url(MOCK_CLIENT_ID, MOCK_CLIENT_SECRET)
+
+
+def _get_stateful_mock_auth_code(init_auth: str) -> str:
+    init_query = parse_qs(urlparse(init_auth).query)
+    return MOCK_AUTH_CODE.format(init_query["state"][0])
 
 
 @pytest.fixture
 def authorized_lib(
-    configured_lib: MSGraph, mocker: MockerFixture, init_auth: str
+    configured_lib: MSGraph,
+    mocker: MockerFixture,
+    init_auth: str,
 ) -> MSGraph:
-    _patch_token_response(mocker, 1)
-    configured_lib.authorize_and_get_token(MOCK_AUTH_CODE)
+    _patch_token_response(configured_lib, mocker, 1)
+    configured_lib.authorize_and_get_token(_get_stateful_mock_auth_code(init_auth))
     return configured_lib
 
 
 def _patch_token_response(
-    mocker: MockerFixture, iteration: int
+    library: MSGraph, mocker: MockerFixture, iteration: int
 ) -> MockerFixture._Patcher:
-    mock_token_response = MagicMock()
-    mock_token_response.status_code = 200
-    mock_token_response.headers = {"Content-Type": "application/json"}
-    mock_token_response.json.return_value = {
-        "token_type": "Bearer",
-        "scope": "%20F".join(PermissionBundle.BASIC.value),
-        "expires_in": 3600,
-        "access_token": MOCK_ACCESS_TOKEN.format(iteration),
-        "refresh_token": MOCK_REFRESH_TOKEN.format(iteration),
-    }
-    config = {"post.return_value": mock_token_response}
-    return mocker.patch("microsoftgraph.client.requests", **config)
+    return _patch_graph_response(
+        library,
+        mocker,
+        {
+            "token_type": "Bearer",
+            "scope": "%20F".join(PermissionBundle.BASIC.value),
+            "expires_in": 3600,
+            "access_token": MOCK_ACCESS_TOKEN.format(iteration),
+            "refresh_token": MOCK_REFRESH_TOKEN.format(iteration),
+        },
+    )
 
 
 def _patch_graph_response(
-    mocker: MockerFixture, return_value: dict
+    library: MSGraph, mocker: MockerFixture, return_value: dict
 ) -> MockerFixture._Patcher:
     mock_graph_response = MagicMock()
     mock_graph_response.status_code = 200
     mock_graph_response.headers = {"Content-Type": "application/json"}
     mock_graph_response.json.return_value = return_value
-    config = {"request.return_value": mock_graph_response}
-    return mocker.patch("microsoftgraph.client.requests", **config)
+    mock_graph_response.text = JSONEncoder().encode(return_value)
+    config = {"return_value": mock_graph_response}
+
+    return mocker.patch.object(library.client.connection.session, "request", **config)
 
 
 def test_configuring_graph_client(library: MSGraph, mocker: MockerFixture) -> None:
-    mock_client = mocker.patch("RPA.MSGraph.Client", autospec=True)
+    mock_client = mocker.patch("RPA.MSGraph.Account", autospec=True)
 
-    library.generate_oauth_authorize_url(MOCK_CLIENT_ID, MOCK_CLIENT_SECRET)
+    library.generate_oauth_authorization_url(MOCK_CLIENT_ID, MOCK_CLIENT_SECRET)
 
-    mock_client.assert_any_call(MOCK_CLIENT_ID, MOCK_CLIENT_SECRET)
+    mock_client.assert_any_call((MOCK_CLIENT_ID, MOCK_CLIENT_SECRET), token_backend=ANY)
 
 
 def test_generating_auth_url(init_auth: str) -> None:
     params = {
+        "response_type": "code",
         "client_id": MOCK_CLIENT_ID,
         "redirect_uri": DEFAULT_REDIRECT_URI,
         "scope": " ".join(PermissionBundle.BASIC.value),
-        "response_type": "code",
-        "response_mode": "query",
-        "state": DEFAULT_STATE,
     }
+    encoded_params = urlencode(params).replace(r"+", r"\+")
+    pattern = re.compile(
+        rf"https:\/\/login.microsoftonline.com\/common\/oauth2\/v2.0\/authorize\?{encoded_params}"
+        r"&state=[a-zA-Z0-9]*&access_type=offline"
+    )
+    result = re.match(pattern, init_auth)
+    assert result
 
-    assert (
-        f"https://login.microsoftonline.com/common/oauth2/v2.0/authorize?{urlencode(params)}"
-        == init_auth
+
+def test_auth_cycle(
+    library: MSGraph,
+    mocker: MockerFixture,
+    init_auth: str,
+) -> None:
+    _patch_token_response(library, mocker, 1)
+
+    refresh_token = library.authorize_and_get_token(
+        _get_stateful_mock_auth_code(init_auth)
     )
 
-
-def test_auth_cycle(library: MSGraph, mocker: MockerFixture, init_auth: str) -> None:
-    _patch_token_response(mocker, 1)
-
-    refresh_token = library.authorize_and_get_token(MOCK_AUTH_CODE)
-
-    assert library.user_token.data["access_token"] == MOCK_ACCESS_TOKEN.format(1)
+    assert library.token_backend.get_token()[
+        "access_token"
+    ] == MOCK_ACCESS_TOKEN.format(1)
     assert refresh_token == MOCK_REFRESH_TOKEN.format(1)
 
 
 def test_refreshing_token(configured_lib: MSGraph, mocker: MockerFixture) -> None:
-    _patch_token_response(mocker, 2)
+    return_token = {
+        "token_type": "Bearer",
+        "expires_in": 3600,
+        "access_token": MOCK_ACCESS_TOKEN.format(2),
+        "refresh_token": MOCK_REFRESH_TOKEN.format(2),
+        "expires_at": time.time() + 3600,
+        "scope": "%20F".join(PermissionBundle.BASIC.value),
+        "scopes": PermissionBundle.BASIC.value,
+    }
+
+    # mock_graph_response = MagicMock(
+    #     spec=OAuth2Token, wraps=return_token, **scope_config
+    # )
+    # mock_graph_response.scope = "%20F".join(PermissionBundle.BASIC.value)
+    # mock_graph_response.scopes = PermissionBundle.BASIC.value
+    # mock_graph_response.__getitem__.side_effect = return_token.__getitem__
+    # mock_graph_response.__iter__.side_effect = return_token.__iter__
+    # mock_graph_response.__contains__.side_effect = return_token.__contains__
+    # mock_graph_response.keys.side_effect = return_token.keys
+    config = {"return_value.refresh_token.return_value": return_token}
+    mocker.patch("O365.connection.OAuth2Session", **config)
 
     refresh_token = configured_lib.refresh_oauth_token(MOCK_REFRESH_TOKEN.format(1))
 
-    assert configured_lib.user_token.data["access_token"] == MOCK_ACCESS_TOKEN.format(2)
+    assert configured_lib.token_backend.get_token()[
+        "access_token"
+    ] == MOCK_ACCESS_TOKEN.format(2)
     assert refresh_token == MOCK_REFRESH_TOKEN.format(2)
 
 
@@ -128,38 +168,96 @@ def test_get_me(authorized_lib: MSGraph, mocker: MockerFixture) -> None:
         "userPrincipalName": "AdeleV@contoso.onmicrosoft.com",
         "id": "87d349ed-44d7-43e1-9a83-5f2406dee5bd",
     }
-    _patch_graph_response(mocker, data)
+    _patch_graph_response(authorized_lib, mocker, data)
 
-    response = authorized_lib.get_me()
+    user_me = authorized_lib.get_me()
 
-    assert response.data == data
+    assert str(user_me) == data["displayName"]
+    assert user_me.object_id == data["id"]
 
 
 @pytest.mark.parametrize(
-    "properties",
+    "search_string,response",
     [
-        "displayName, givenName,postalCode, identities",
-        ["displayName", "givenName", "postalCode", "identities"],
+        (
+            "adam",
+            {
+                "@odasta.context": "https://graph.microsoft.com/v1.0/$metadata#users",
+                "value": [
+                    {
+                        "businessPhones": [],
+                        "displayName": "Conf Room Adams",
+                        "givenName": None,
+                        "jobTitle": None,
+                        "mail": "Adams@contoso.com",
+                        "mobilePhone": None,
+                        "officeLocation": None,
+                        "preferredLanguage": None,
+                        "surname": None,
+                        "userPrincipalName": "Adams@contoso.com",
+                        "id": "6ea91a8d-e32e-41a1-b7bd-d2d185eed0e0",
+                    },
+                    {
+                        "businessPhones": ["425-555-0100"],
+                        "displayName": "Adam Administrator",
+                        "givenName": "Adam-adm",
+                        "jobTitle": None,
+                        "mail": None,
+                        "mobilePhone": "425-555-0101",
+                        "officeLocation": None,
+                        "preferredLanguage": "en-US",
+                        "surname": "Administrator",
+                        "userPrincipalName": "admin@contoso.com",
+                        "id": "4562bcc8-c436-4f95-b7c0-4f8ce89dca5e",
+                    },
+                ],
+            },
+        ),
+        (
+            "john",
+            {
+                "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#users",
+                "value": [
+                    {
+                        "businessPhones": ["555-555-0100"],
+                        "displayName": "Johnny Apple",
+                        "givenName": "John",
+                        "jobTitle": "IT Admin",
+                        "mail": "j.apple@contoso.com",
+                        "mobilePhone": None,
+                        "officeLocation": None,
+                        "preferredLanguage": None,
+                        "surname": None,
+                        "userPrincipalName": "j.apple@contoso.com",
+                        "id": "6ea91a8d-e32e-41a1-b7bd-d2d185eed123",
+                    },
+                    {
+                        "businessPhones": ["555-123-0100"],
+                        "displayName": "John Smith",
+                        "givenName": "John",
+                        "jobTitle": "BDR",
+                        "mail": "j.smith@contoso.com",
+                        "mobilePhone": "555-123-0101",
+                        "officeLocation": None,
+                        "preferredLanguage": "en-US",
+                        "surname": "Administrator",
+                        "userPrincipalName": "admin@contoso.com",
+                        "id": "4562bcc8-c436-4f95-b7c0-4f8ce89dc123",
+                    },
+                ],
+            },
+        ),
     ],
 )
-def test_get_me_with_properties(
-    authorized_lib: MSGraph, mocker: MockerFixture, properties: Union[str, list[str]]
+def test_search_for_users(
+    authorized_lib: MSGraph, mocker: MockerFixture, search_string: str, response: dict
 ) -> None:
-    data = {
-        "@odata.context": "https://graph.microsoft.com/v1.0/$metadata#users(displayName,givenName,postalCode,identities)/$entity",
-        "displayName": "Adele Vance",
-        "givenName": "Adele",
-        "postalCode": "98004",
-        "identities": [
-            {
-                "signInType": "userPrincipalName",
-                "issuer": "contoso.com",
-                "issuerAssignedId": "AdeleV@contoso.com",
-            }
-        ],
-    }
-    _patch_graph_response(mocker, data)
+    _patch_graph_response(authorized_lib, mocker, response)
 
-    response = authorized_lib.get_me(properties)
+    users = authorized_lib.search_for_users(search_string)
 
-    assert response.data == data
+    for user in users:
+        assert user.display_name in [u["displayName"] for u in response["value"]]
+        assert user.user_principal_name in [
+            u["userPrincipalName"] for u in response["value"]
+        ]
